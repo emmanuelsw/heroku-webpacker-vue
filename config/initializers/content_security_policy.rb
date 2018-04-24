9fe235ca42a22a9ad28ecd2ca6982cbd1de77df7
EmmanuelSW <edhgt89@hotmail.com>
2018-04-24 14:36:53 -0500

    Initial commit

---
+++ config/initializers/content_security_policy.rb
@@ -0,0 +1,22 @@
+# Be sure to restart your server when you modify this file.
+
+# Define an application-wide content security policy
+# For further information see the following documentation
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
+
+Rails.application.config.content_security_policy do |p|
+	if Rails.env.development?
+		p.script_src :self, :https, :unsafe_eval
+		p.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035'
+	else
+		p.script_src :self, :https
+	end
+end
+
+# If you are using UJS then enable automatic nonce generation
+# Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
+
+# Report CSP violations to a specified URI
+# For further information see the following documentation:
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
+# Rails.application.config.content_security_policy_report_only = true
